7bcbfe88fccb7b9480c6daac10c28418fa070a44
LRTRC <loirthomas.pro@gmail.com>
2022-06-18 17:07:14 +0200

    creating component customCard

--- pages/home.vue
+++ pages/home.vue
@@ -1,10 +1,22 @@
 <template>
+<v-card >
+  <h1>
+    TITLE
+  </h1>
+  <v-card-subtitle>
+    subtitle
+  </v-card-subtitle>
+</v-card>
+<v-divider />
+<customCard />
 
 </template>
 
 <script>
+import {customCard} from '../components/home/customCard';
 export default {
-  name: "home"
+  name: "home",
+  components: []
 }
 </script>
 
