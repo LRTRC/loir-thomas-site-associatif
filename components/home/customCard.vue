c44ace28d5eb4cc71302ffa3a5fe8d79ad8a811a
LRTRC <loirthomas.pro@gmail.com>
2022-06-23 15:05:39 +0200

    grid complete without css

--- components/home/customCard.vue
+++ components/home/customCard.vue
@@ -1,10 +1,34 @@
 <template>
-
+  <v-card >
+    <v-img
+      :src="src"
+    />
+    <h3>
+      {{ subtitle }}
+    </h3>
+    <p>
+      {{ paragraph }}
+    </p>
+  </v-card>
 </template>
 
 <script>
 export default {
-  name: "customCard"
+  name: "customCard",
+  props: {
+    src: {
+      type: String,
+      default: '../blank_img.jpeg',
+    },
+    subtitle: {
+      type: String,
+      default: '',
+    },
+    paragraph: {
+      type: String,
+      default: '',
+    }
+  }
 }
 </script>
 
