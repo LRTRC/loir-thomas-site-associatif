e2b6d6af6642e2f261378b20e647c39ea568dee4
LRTRC <97665266+LRTRC@users.noreply.github.com>
2022-07-07 10:49:17 +0200

    [ Components / agenda ] init page and components (#8)
    * setting agenda page
    
    * missing search function and filters to refact not scalable
    
    Co-authored-by: root <root@DESKTOP-I516SA9.localdomain>

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - loir-thomas-site-web-associatif',
-    title: 'loir-thomas-site-web-associatif',
+    title: 'TOURANGEAU GYM CLUB',
     htmlAttrs: {
       lang: 'en'
     },
--- pages/agenda.vue
+++ pages/agenda.vue
@@ -1,13 +1,103 @@
 <template>
+  <div>
 
+    <v-row>
+      <v-col>
+        <CustomHeader :title="headerTitle" :subtitle="headerSubTitle" />
+      </v-col>
+    </v-row>
+
+    <v-divider />
+
+    <v-row justify="center">
+      <v-col cols="12" md="10">
+        <iframe class="map" height="100%" frameborder="10" width="100%" scrolling="no"
+          src="https://maps.google.fr/maps?f=q&amp;source=s_q&amp;hl=fr&amp;geocode=&amp;q=22+Boulevard+Béranger 37000 Tours&amp;vpsrc=1&amp;output=embed"
+          marginwidth="0" marginheight="0">
+        </iframe>
+      </v-col>
+    </v-row>
+
+    <v-divider />
+
+    <v-row>
+      <v-col>
+        <v-text-field v-model="searchField" label="Rechercher ..." clearable></v-text-field>
+      </v-col>
+      <v-col>
+        <v-select v-model="selectedTheme" :items="themes" label="Thèmes" clearable></v-select>
+      </v-col>
+      <v-col>
+        <v-menu v-model="datePickerMenu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition"
+          offset-y min-width="auto">
+          <template v-slot:activator="{ on, attrs }">
+            <v-text-field v-model="date" label="Choisir une date" prepend-icon="mdi-calendar" readonly v-bind="attrs"
+              v-on="on" clearable></v-text-field>
+          </template>
+          <v-date-picker v-model="date" @input="datePickerMenu = false"></v-date-picker>
+        </v-menu>
+      </v-col>
+    </v-row>
+
+    <v-divider />
+
+    <v-row justify="center">
+      <v-col cols="12" sm="6" md="4"
+        v-if="selectedTheme === null && date === null || selectedTheme === 'portes ouvertes' && date === '2022-09-12' || date === '2022-09-12' && selectedTheme === null || selectedTheme === 'portes ouvertes' && date === null">
+        <CustomCard :subtitle="subtitles[0]" :paragraph="paragraphs[0]" :src="src[0]" />
+      </v-col>
+      <v-col cols="12" sm="6" md="4"
+        v-if="selectedTheme === null && date === null || selectedTheme === 'compétition' && date === '2022-11-18' || date === '2022-11-18' && selectedTheme === null || selectedTheme === 'compétition' && date === null">
+        <CustomCard :subtitle="subtitles[1]" :paragraph="paragraphs[1]" :src="src[1]" />
+      </v-col>
+      <v-col cols="12" sm="6" md="4"
+        v-if="selectedTheme === null && date === null || selectedTheme === 'exhibition' && date === '2022-08-05' || date === '2022-08-05' && selectedTheme === null || selectedTheme === 'exhibition' && date === null">
+        <CustomCard :subtitle="subtitles[2]" :paragraph="paragraphs[2]" :src="src[2]" />
+      </v-col>
+    </v-row>
+
+  </div>
 </template>
 
 <script>
+import CustomHeader from '../components/customHeader.vue';
+import CustomCard from '../components/home/customCard.vue';
 export default {
-  name: "agenda"
+  name: "agenda",
+  components: { CustomHeader, CustomCard },
+  data() {
+    return {
+      searchField: '',
+      selectedTheme: null,
+      themes: ['portes ouvertes', 'compétition', 'exhibition'],
+      datePickerMenu: false,
+      date: null,
+      headerTitle: 'tourangeau gym club',
+      headerSubTitle: 'Listes des évènements',
+      src: [
+        '../open-day.jpeg',
+        '../open-crossfit.jpeg',
+        '../exhibition.jpeg',
+      ],
+      subtitles: [
+        "Portes ouvertes 2022",
+        "Open CrossFit du Val de Loire 2022",
+        "Démonstration publique de pilates",
+
+      ],
+      paragraphs: [
+        "Le GYM Club sonne la cloche de la rentrée samedi 12 septembre 2022. Le gymnase ouvrira grand " +
+        "ses portes au public de 14 h à 17 h pour les inscriptions. Loisirs, compétition, performance pour adultes et enfants",
+        "L'Open CrossFit du Val de Loire se tiendra le 18 novembre 2022 et donnera aux athlètes CrossFit de toute la région la chance de se mettre au défi et " +
+        "de briller sur la scène française.",
+        "Rencontrez vos professionnels du bien-être et tester gratuitement ces activités" +
+        "L'occasion de rappeler les bienfaits du pilates sur le corps. RDV le 5 août 2022 à 14h au parc Montsouris"
+      ],
+    }
+  },
 }
 </script>
 
-<style scoped>
 
+<style scoped>
 </style>
