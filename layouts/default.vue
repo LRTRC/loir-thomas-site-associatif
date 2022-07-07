e8a34900a903812ed910df94430c181660558469
root <root@DESKTOP-I516SA9.localdomain>
2022-07-07 15:21:11 +0200

    deleting drawer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,19 +1,6 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
-      <v-list>
-        <v-list-item v-for="(route, i) in routes" :key="i" :to="route.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ route.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="route.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+    <v-app-bar fixed app>
       <v-spacer />
       <v-row justify="center">
         <v-col>
