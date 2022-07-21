ff5d2ac28544e37653c40f8acee2eddbff223cfd
root <root@DESKTOP-I516SA9.localdomain>
2022-07-21 10:42:51 +0200

    page agenda done

--- pages/agenda.vue
+++ pages/agenda.vue
@@ -2,14 +2,12 @@
   <div>
 
     <v-row>
-      <v-col  class="pt-0 px-0">
-        <CustomHeader :title="headerTitle" :subtitle="headerSubTitle" />
+      <v-col class="pt-0 px-0">
+        <CustomHeader :title="titles[0]" :subtitle="subtitles"/>
       </v-col>
     </v-row>
 
-    <v-divider />
-
-    <v-row justify="center">
+    <v-row class="my-4" justify="center">
       <v-col cols="12" md="10">
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2701.1418288301124!2d0.6823868335054476!3d47.38966554431728!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47fcd5b7b725aec5%3A0x652b0c6527606376!2s22%20Bd%20B%C3%A9ranger%2C%2037000%20Tours!5e0!3m2!1sfr!2sfr!4v1657200550848!5m2!1sfr!2sfr"
@@ -18,41 +16,113 @@
       </v-col>
     </v-row>
 
-    <v-divider />
+    <v-row justify="center" class="my-4">
 
-    <v-row>
-      <v-col>
-        <v-select v-model="selectedTheme" :items="themes" label="Thèmes" clearable></v-select>
-      </v-col>
-      <v-col>
-        <v-menu v-model="datePickerMenu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition"
-          offset-y min-width="auto">
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field v-model="date" label="Choisir une date" prepend-icon="mdi-calendar" readonly v-bind="attrs"
-              v-on="on" clearable></v-text-field>
-          </template>
-          <v-date-picker v-model="date" @input="datePickerMenu = false"></v-date-picker>
-        </v-menu>
-      </v-col>
-    </v-row>
+      <v-col cols="10" md="8">
+        <v-card flat>
 
-    <v-divider />
+          <v-row class="my-4" justify="center">
+            <v-col class="text-center">
+              <h2>
+                {{ titles[1].toUpperCase() }}
+              </h2>
+            </v-col>
+          </v-row>
 
-    <v-row justify="center">
-      <v-col cols="12" sm="6" md="4"
-        v-if="selectedTheme === null && date === null || selectedTheme === 'portes ouvertes' && date === '2022-09-12'
-         || date === '2022-09-12' && selectedTheme === null || selectedTheme === 'portes ouvertes' && date === null">
-        <CustomCard :subtitle="subtitles[0]" :paragraph="paragraphs[0]" :src="src[0]" />
-      </v-col>
-      <v-col cols="12" sm="6" md="4"
-        v-if="selectedTheme === null && date === null || selectedTheme === 'compétition' && date === '2022-11-18'
-        || date === '2022-11-18' && selectedTheme === null || selectedTheme === 'compétition' && date === null">
-        <CustomCard :subtitle="subtitles[1]" :paragraph="paragraphs[1]" :src="src[1]" />
-      </v-col>
-      <v-col cols="12" sm="6" md="4"
-        v-if="selectedTheme === null && date === null || selectedTheme === 'exhibition' && date === '2022-08-05'
-        || date === '2022-08-05' && selectedTheme === null || selectedTheme === 'exhibition' && date === null">
-        <CustomCard :subtitle="subtitles[2]" :paragraph="paragraphs[2]" :src="src[2]" />
+          <v-window
+            v-model="onBoarding"
+          >
+            <v-window-item
+              v-for="(event, i) in events"
+              :key="i">
+              <v-card
+                height="100%"
+                width="100%"
+                flat
+              >
+                <v-row class="ma-4">
+                  <v-col cols="12" md="6">
+                    <img
+                      height="100%"
+                      width="100%"
+                      :src="event.img"
+                    />
+                  </v-col>
+                  <v-col cols="12" md="6">
+                    <v-row>
+                      <v-col>
+                        <p class="py-4 text-left">
+                          {{ event.text }}
+                        </p>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col class="text-right">
+                        <h3>
+                          {{ event.title }}
+                        </h3>
+                      </v-col>
+                    </v-row>
+                    <v-row>
+                      <v-col class="text-right">
+                        <p>
+                          <span class="events">{{ event.theme }}</span>
+                        </p>
+                      </v-col>
+                    </v-row>
+                  </v-col>
+                </v-row>
+
+              </v-card>
+            </v-window-item>
+          </v-window>
+
+          <v-card-actions class="justify-space-between">
+            <v-btn
+              text
+              @click="prev"
+            >
+              <v-icon
+                x-large
+                color="primary"
+              >
+                {{ icons[0] }}
+              </v-icon>
+            </v-btn>
+            <v-item-group
+              v-model="onBoarding"
+              class="text-center"
+              mandatory
+            >
+              <v-item
+                v-for="(event, i) in events"
+                :key="i"
+                v-slot="{ active, toggle }"
+              >
+                <v-btn
+                  :input-value="active"
+                  icon
+                  @click="toggle"
+                >
+                  <v-icon>
+                    {{ icons[2] }}
+                  </v-icon>
+                </v-btn>
+              </v-item>
+            </v-item-group>
+            <v-btn
+              text
+              @click="next"
+            >
+              <v-icon
+                x-large
+                color="primary"
+              >
+                {{ icons[1] }}
+              </v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
 
@@ -62,43 +132,66 @@
 <script>
 import CustomHeader from '../components/customHeader.vue';
 import CustomCard from '../components/customCard.vue';
+import {mdiChevronLeft, mdiChevronRight, mdiRecord} from '@mdi/js';
 
 export default {
   name: "agenda",
-  components: { CustomHeader, CustomCard },
+  components: {CustomHeader, CustomCard},
   data() {
     return {
+      onBoarding: 0,
+      icons: [mdiChevronLeft, mdiChevronRight, mdiRecord],
       searchField: '',
       selectedTheme: null,
-      themes: ['portes ouvertes', 'compétition', 'exhibition'],
-      datePickerMenu: false,
-      date: null,
-      headerTitle: 'tourangeau gym club',
-      headerSubTitle: 'Listes des évènements',
-      src: [
-        '../open-day.jpeg',
-        '../open-crossfit.jpeg',
-        '../exhibition.jpeg',
-      ],
-      subtitles: [
-        "Portes ouvertes 2022",
-        "Open CrossFit du Val de Loire 2022",
-        "Démonstration publique de pilates",
-
-      ],
-      paragraphs: [
-        "Le GYM Club sonne la cloche de la rentrée samedi 12 septembre 2022. Le gymnase ouvrira grand " +
-        "ses portes au public de 14 h à 17 h pour les inscriptions. Loisirs, compétition, performance pour adultes et enfants",
-        "L'Open CrossFit du Val de Loire se tiendra le 18 novembre 2022 et donnera aux athlètes CrossFit de toute la région la chance de se mettre au défi et " +
-        "de briller sur la scène française.",
-        "Rencontrez vos professionnels du bien-être et tester gratuitement ces activités" +
-        "L'occasion de rappeler les bienfaits du pilates sur le corps. RDV le 5 août 2022 à 14h au parc Montsouris"
+      titles: ['tourangeau gym club', 'évènements'],
+      subtitles: 'Listes des évènements',
+      events: [
+        {
+          title: 'Portes ouvertes 2022',
+          text: "Le GYM Club sonne la cloche de la rentrée samedi 12 septembre 2022. Le gymnase ouvrira grand " +
+            "ses portes au public de 14 h à 17 h pour les inscriptions. Loisirs, compétition, performance pour adultes et enfants",
+          theme: 'portes ouvertes',
+          img: '../open-day.jpeg',
+        },
+        {
+          title: 'Open CrossFit du Val de Loire 2022',
+          text: "L'Open CrossFit du Val de Loire se tiendra le 18 novembre 2022 et donnera aux athlètes CrossFit de toute la région la chance de se mettre au défi et " +
+            "de briller sur la scène française.",
+          theme: 'compétition',
+          img: '../open-crossfit.jpeg',
+        },
+        {
+          title: 'Démonstration publique de pilates',
+          text: "Rencontrez vos professionnels du bien-être et tester gratuitement ces activités" +
+            "L'occasion de rappeler les bienfaits du pilates sur le corps. RDV le 5 août 2022 à 14h au parc Montsouris",
+          theme: 'exhibition',
+          img: '../exhibition.jpeg',
+        },
       ],
     }
+  },
+  methods: {
+    next() {
+      this.onBoarding = this.onBoarding + 1 === this.eventes
+        ? 0
+        : this.onBoarding + 1
+    },
+    prev() {
+      this.onBoarding = this.onBoarding - 1 < 0
+        ? this.eventes - 1
+        : this.onBoarding - 1
+    },
   },
 }
 </script>
 
 
 <style scoped>
+.events {
+  color: #662D91;
+  font-family: 'Barlow-Black', sans-serif;
+  font-style: italic;
+  font-size: 1em !important;
+  letter-spacing: 5px !important;
+}
 </style>
